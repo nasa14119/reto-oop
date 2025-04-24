b5ebc17117b8cb52c2d21a8b1d5b8e062782224a
nasa14119 <nico.amaya0205@gmail.com>
2025-04-23 19:33:54 -0600

    feat(utils): method to get data of movies from file

--- utils.cpp
+++ utils.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+#include <sstream>
 #include "utils.h"
+#include "clases/Movies.cpp"
 using namespace std;
-
+extern const string MOVIES_str;
+extern const string SERIES_str;
 string utils::get_file_path(string str)
 {
   filesystem::path file(str);
   return filesystem::canonical(file);
-}
+};
 void utils::clear()
 {
   cout << "\033[2J\033[H" << flush;
+};
+MediaVector utils::get_data_movies(MediaVector &medias, string str)
+{
+  string path_movies = utils::get_file_path(str);
+  string values[6];
+  string line;
+  ifstream file_movie(path_movies);
+  if (!file_movie.is_open())
+  {
+    throw "Error reading file";
+  }
+  while (getline(file_movie, line))
+  {
+    if (line == MOVIES_str)
+    {
+      break;
+    }
+    stringstream ss(line);
+    int i = 0;
+    while (getline(ss, values[i], ','))
+    {
+      i++;
+    }
+    medias.emplace_back(new Movies(values[0], values[1], values[2], stoi(values[3]), stof(values[4]), values[5]));
+  }
+  file_movie.close();
+  return medias;
 };
--- utils.h
+++ utils.h
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <filesystem>
+#include "clases/Media.h"
 using namespace std;
+extern const string MOVIES_str;
+extern const string SERIES_str;
 class utils
 {
 public:
   static string get_file_path(string str);
   static void clear();
+  static MediaVector get_data_movies(MediaVector &, string);
 };
