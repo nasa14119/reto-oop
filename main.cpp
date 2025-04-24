9706a7adf945d4dd9ffbf01f9752dafcc23561ea
nasa14119 <nico.amaya0205@gmail.com>
2025-04-23 19:38:57 -0600

    feat(utils): adding method to get inoformation from movies file

--- main.cpp
+++ main.cpp
@@ -8,38 +8,17 @@
 #include "clases/Controller.cpp"
 #include "clases/Filters.cpp"
 using namespace std;
+const string MOVIES_str = "movies";
+const string SERIES_str = "series";
 void file_setup(MediaVector &media)
 {
-  string values[6];
-  string line;
-  string path_movies = utils::get_file_path("../movies.csv");
-  string path_series = utils::get_file_path("../series.csv");
-
-  ifstream file_movie(path_movies);
-  if (!file_movie.is_open())
-  {
-    throw "Error reading file";
-  }
-  while (getline(file_movie, line))
-  {
-    stringstream ss(line);
-    int i = 0;
-    while (getline(ss, values[i], ','))
-    {
-      i++;
-    }
-    media.emplace_back(new Movies(values[0], values[1], values[2], stoi(values[3]), stof(values[4]), values[5]));
-  }
-  file_movie.close();
+  utils::get_data_movies(media, "../movies.csv");
 };
 int main()
 {
-  // utils::clear();
   MediaVector media;
   file_setup(media);
   Controller control(&media);
-  // control.print_movies();
-  MediaVector filter_movies = filters::filter_by_title(media);
-  Controller::print_movies(&filter_movies);
+  control.print_movies();
   return 0;
 }
